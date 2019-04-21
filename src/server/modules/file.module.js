6d4d469c69aa2cb6d1e007db56d6767b35c8bdbb
littlehorseboy <littlehorseboy@gmail.com>
2019-04-22 00:12:03 +0800

    feat: insertOrUpdateFile

--- src/server/modules/file.module.js
+++ src/server/modules/file.module.js
@@ -29,6 +29,37 @@ const selectFile = fileId => new Promise((resolve, reject) => {
   });
 });
 
+const insertOrUpdateFile = file => new Promise((resolve, reject) => {
+  MongoClient.connect(mLabUrl, { useNewUrlParser: true }, (connectError, client) => {
+    if (connectError) {
+      reject(connectError);
+    }
+    assert.strictEqual(connectError, null);
+
+    const collection = client.db(mLabDBName).collection('files');
+
+    collection.findOne({ fileId: file.fileId })
+      .then((result) => {
+        if (result !== null) {
+          collection.updateOne({ fileId: file.fileId }, { file })
+            .then(updateResult => resolve(updateResult))
+            .catch(updateError => reject(updateError));
+        } else {
+          collection.insertOne({ file })
+            .then(insertResult => resolve(insertResult))
+            .catch(insertError => reject(insertError));
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      })
+      .then(() => {
+        client.close();
+      });
+  });
+});
+
 export default {
   selectFile,
+  insertOrUpdateFile,
 };
